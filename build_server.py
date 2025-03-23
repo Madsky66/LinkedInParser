9433ed1de1cd0dbec906b7c8eb1ac409e9d20f1c
Madsky66 <pmbussy66@gmail.com>
2025-03-24 00:34:31 +0100

    Add build_server.py to compile server.py into a standalone executable using PyInstaller

---
+++ build_server.py
@@ -0,0 +1,36 @@
+import PyInstaller.__main__
+import os
+import sys
+
+def build_server():
+    """Compile le serveur Python en executable"""
+    # Chemin absolu vers le script server.py
+    script_path = os.path.join(os.getcwd(), 'src', 'main', 'python', 'server.py')
+    output_path = os.path.join(os.getcwd(), 'src', 'main', 'resources', 'extra')
+
+    # Assurez-vous que le dossier de sortie existe
+    os.makedirs(output_path, exist_ok=True)
+
+    PyInstaller.__main__.run([
+        script_path,
+        '--onefile',
+        '--name=server',
+        f'--distpath={output_path}',
+        '--noconsole',
+        '--hidden-import=websockets',
+        '--hidden-import=httpx',
+        '--hidden-import=beautifulsoup4',
+        '--hidden-import=fake_useragent',
+        '--hidden-import=cloudscraper',
+        '--hidden-import=undetected_chromedriver',
+        '--hidden-import=selenium',
+        '--hidden-import=asyncio',
+        '--hidden-import=logging',
+        '--hidden-import=json',
+        '--hidden-import=random',
+        '--hidden-import=time',
+        '--hidden-import=bs4',
+    ])
+
+if __name__ == "__main__":
+    build_server()
