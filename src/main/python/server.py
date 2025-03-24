238142b1927d4840da21af2c667c51574190dc8d
Madsky66 <pmbussy66@gmail.com>
2025-03-24 19:33:05 +0100

    Refactor server.py to enhance Selenium integration with WebDriverWait and expected_conditions

--- src/main/python/server.py
+++ src/main/python/server.py
@@ -4,6 +4,9 @@ import logging
 import asyncio
 import websockets
 import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # Configuration du logging
 logging.basicConfig(
