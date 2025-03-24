71cc852823fbcf5ce930aa479aa3a36a711eb8af
Madsky66 <pmbussy66@gmail.com>
2025-03-24 17:15:03 +0100

    Update Compose plugin version to 1.7.3 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
-    id("org.jetbrains.compose") version "1.5.15"
+    id("org.jetbrains.compose") version "1.7.3"
 }
 
 group = "com.madsky"
