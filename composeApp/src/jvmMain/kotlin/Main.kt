1814418c9c74bec89ecb165932defda3c7352335
Madsky66 <pmbussy66@gmail.com>
2025-04-06 18:50:22 +0200

    Refactor Main.kt; streamline import statements and enhance variable usage for improved readability and maintainability

--- composeApp/src/jvmMain/kotlin/Main.kt
+++ composeApp/src/jvmMain/kotlin/Main.kt
@@ -1,27 +1,10 @@
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.window.WindowDraggableArea
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.ModalDrawer
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Window
+import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
@@ -66,28 +49,29 @@ fun main() = application {
 
 @Composable
 fun AppTitleBar(applicationScope: CoroutineScope) {
-    var isMinimized = mutableStateOf(gC.windowState.value.isMinimized)
-    val isMaximized = gC.isWindowMaximized
+    val lightGray = gC.lightGray.value
+    var isMinimized by remember {mutableStateOf(gC.windowState.value.isMinimized)}
+
     // Titre
     Row(Modifier.fillMaxHeight(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
         // Icone de menu
-        IconButton({applicationScope.launch {if (gC.drawerState.value.isOpen) gC.drawerState.value.close() else gC.drawerState.value.open()}}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.Menu, "Menu", tint = gC.lightGray.value)}
+        IconButton({applicationScope.launch {if (gC.drawerState.value.isOpen) gC.drawerState.value.close() else gC.drawerState.value.open()}}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.Menu, "Menu", tint = lightGray)}
         // Spacer
         Spacer(Modifier.width(15.dp))
         // Texte
-        Text("LinkedIn Parser", fontSize = 15.sp, color = gC.lightGray.value)
+        Text("LinkedIn Parser", fontSize = 15.sp, color = lightGray)
     }
     // Boutons
     Row(Modifier.fillMaxHeight(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
         // Minimiser
-        IconButton({isMinimized.value = true}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.KeyboardArrowDown, "Minimiser", tint = gC.lightGray.value)}
+        IconButton({isMinimized = true}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.KeyboardArrowDown, "Minimiser", tint = lightGray)}
         // Spacer
         Spacer(Modifier.width(15.dp))
         // Maximiser / Restaurer
-        IconButton({gC.windowState.value.placement = if (isMinimized.value) WindowPlacement.Maximized else WindowPlacement.Floating; isMaximized.value = !isMaximized.value}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.Window, "Maximiser / Restaurer", tint = gC.lightGray.value)}
+        IconButton({gC.windowState.value.placement = if (isMinimized) WindowPlacement.Maximized else WindowPlacement.Floating; gC.isWindowMaximized.value = !gC.isWindowMaximized.value}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.Window, "Maximiser / Restaurer", tint = lightGray)}
         // Spacer
         Spacer(Modifier.width(15.dp))
         // Quitter
-        IconButton({exitProcess(0)}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.Close, "Quitter", tint = gC.lightGray.value)}
+        IconButton({exitProcess(0)}, Modifier.size(25.dp).clip(RoundedCornerShape(100))) {Icon(Icons.Filled.Close, "Quitter", tint = lightGray)}
     }
 }
